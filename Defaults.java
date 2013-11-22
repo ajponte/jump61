5dc4f376ba32989e622f603a32afc5b51aacf6bb
Alan Ponte <alanjponte@gmail.com>
2013-11-22 00:01:36 -0800

    Create Defaults.java
    Collected constants used as default or initial values.

---
+++ Defaults.java
@@ -0,0 +1,31 @@
+package jump61;
+
+/** Collected constants used as default or initial values.
+ *  @author Alan Ponte
+ */
+class Defaults {
+
+    /** Current version designator. */
+    static final String VERSION = "Jump61 2.0";
+
+    /** Default board size. */
+    static final int BOARD_SIZE = 6;
+
+    /** Default player1 color is BLUE.*/
+    static final Color PLAYER1 = Color.BLUE;
+
+    /** Default player2 color is is RED.*/
+    static final Color PLAYER2 = Color.RED;
+
+    /** The default depth of the game tree.*/
+    static final int DEPTH = 4;
+
+    /** The default number of players in the game.*/
+    static final int NUMPLAYERS = 2;
+
+    /** The default color of any square is White.*/
+    static final Color BLANK_COLOR = Color.WHITE;
+
+    /** Every suare is initialiazed with 0 spots.*/
+    static final int INIT_SPOTS = 0;
+}
