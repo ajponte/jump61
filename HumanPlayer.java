dde86e0ad55b5d33b3f27a62ffa8373c01c2d8d1
Alan Ponte <alanjponte@gmail.com>
2013-11-22 00:04:50 -0800

    Create HumanPlayer.java

---
+++ HumanPlayer.java
@@ -0,0 +1,23 @@
+package jump61;
+
+
+/** A Player that gets its moves from manual input.
+ *  @author Alan Ponte
+ */
+class HumanPlayer extends Player {
+
+    /** A new player initially playing COLOR taking manual input of
+     *  moves from GAME's input source. */
+    HumanPlayer(Game game, Color color) {
+        super(game, color);
+    }
+
+    @Override
+    void makeMove() {
+        Game game = getGame();
+        int[] move = new int[2];
+        game.getMove(move);
+        game.makeMove(move[0], move[1]);
+    }
+
+}
